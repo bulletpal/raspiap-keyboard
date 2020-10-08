4e60e7a61e5f6273b3ccf7eaf051b0147082f31a
bulletpal <jacob.w.gable@gmail.com>
2020-10-08 22:30:00 +0100

    working keyboard turn-on

--- bt_connection_listener.py
+++ bt_connection_listener.py
@@ -1,5 +1,17 @@
 import os
+import time
 
-devices = os.popen("bluetoothctl info 34:88:5D:81:35:E8").read()
+connected = False
+lastconnected = False
 
-print(devices[devices.find("Connected:"):])
+while 1:
+	device = os.popen("bluetoothctl info 34:88:5D:81:35:E8").read()
+	linenumber = device.find("Connected:")
+	if device[linenumber+11:linenumber+13] == "no":
+		connected = False
+	else:
+		connected = True
+	if lastconnected == False and connected == True:
+		os.system("python3 /etc/tv-keyboard-ctrl/btctrl.py")
+	lastconnected = connected
+	time.sleep(1)
